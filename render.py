b84d5d8fd75d5c1ab048b07c65a1a7960cd5d6fd
Jiahui Xiong <ArielHeleneto@outlook.com>
2022-10-07 19:49:59 +0800

    修复了编码问题。

--- render.py
+++ render.py
@@ -2,11 +2,11 @@ from distutils.dist import Distribution
 import pandas
 import yaml
 
-Distribution=["种类", "Debian", "Gentoo", "openSUSE", "Arch Linux", "FreeBSD", "OpenBSD", "Alpine", "openKylin", "Fedora", "OpenEuler", "Anolis", "Deepin", "Ubuntu" ]
-with open("./evidence/observe.yaml", 'r') as stream:
+Distribution = ["种类", "Debian", "Gentoo", "openSUSE", "Arch Linux", "FreeBSD","OpenBSD", "Alpine", "openKylin", "Fedora", "OpenEuler", "Anolis", "Deepin", "Ubuntu"]
+with open("./evidence/observe.yaml", 'r', encoding='utf-8') as stream:
     try:
-        parsed_yaml=yaml.safe_load(stream)
-        # print(parsed_yaml)
+        parsed_yaml = yaml.safe_load(stream)
+        #print(parsed_yaml)
     except yaml.YAMLError as exc:
         print(exc)
 
@@ -19,11 +19,11 @@ for packageCategory in index:
     keys.append(packageCategory)
     df = pandas.DataFrame(columns=Distribution)
     for packageName in parsed_yaml[packageCategory].keys():
-        df = df.append(pandas.DataFrame(parsed_yaml[packageCategory][packageName], index = [packageName])).fillna('N/A')
+        df = df.append(pandas.DataFrame(parsed_yaml[packageCategory][packageName], index=[packageName])).fillna('N/A')
     df["种类"] = packageCategory
     print(df)
     frames.append(df)
 result = pandas.concat(frames, names=['Series name', 'Row ID'])
 
-with open('table.md', "w+") as output :
+with open('table.md', "w+", encoding='utf-8') as output:
     output.write(result.to_markdown())
